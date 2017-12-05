4190342d18af05925ae0e7476c48a683897335e3
Bogdan Popa <popa.bogdanp@gmail.com>
2017-12-05 11:43:42 +0200

    fix: make Warehouse fail after processing

--- cart/warehouse.py
+++ cart/warehouse.py
@@ -6,7 +6,7 @@ class Warehouse:
     @classmethod
     def prepare(cls, *skus):
         for sku in skus:
-            if random.randint(0, 9) < 3:
-                raise RuntimeError("The warehouse system is down.")
-
             time.sleep(1)
+
+        if random.randint(0, 9) < 3:
+            raise RuntimeError("The warehouse system is down.")
